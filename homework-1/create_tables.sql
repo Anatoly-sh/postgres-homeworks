0091eab7752d5420e70f77534af26b988cc5303e
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-04-08 14:56:15 +0300

    скрипт sql + main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,36 @@
 -- SQL-команды для создания таблиц
+DROP TABLE IF EXISTS orders, employees, customers;
+
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name char(30) NOT NULL,
+	last_name char(30) NOT NULL,
+	title char(100) NOT NULL,
+	birst_date date NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name char(50) NOT NULL,
+	contact_name char(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5),
+	FOREIGN KEY(customer_id)
+	REFERENCES customers(customer_id),
+	employee_id serial,
+	FOREIGN KEY(employee_id)
+	REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city char(30) NOT NULL
+);
+
+SELECT * FROM orders;
+SELECT * FROM employees;
+SELECT * FROM customers;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,42 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+# connect to db
+conn = psycopg2.connect(host='localhost', port=5433, database='north', user='postgres', password='12345')
+try:
+    with conn:
+        cur = conn.cursor()
+        # работники (employees)
+        with open('north_data/employees_data.csv') as file:
+            data = [tuple(line) for line in csv.reader(file)]  # список кортежей
+            for item in range(1, len(data)):
+                # execute query
+                cur.execute("INSERT INTO employees VALUES (default, %s, %s, %s, %s, %s)", data[item])
+        # покупатели (customers)
+        with open('north_data/customers_data.csv') as file:
+            data = [tuple(line) for line in csv.reader(file)]  # список кортежей
+            for item in range(1, len(data)):
+                # execute query
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", data[item])
+        # заказы (orders)
+        with open('north_data/orders_data.csv') as file:
+            data = [tuple(line) for line in csv.reader(file)]  # список кортежей
+            for item in range(1, len(data)):
+                # execute query
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", data[item])
+finally:
+    conn.close()
+
+
+# conn = psycopg2.connect(host='localhost', port=5433, database='north', user='postgres', password='12345')
+# try:
+#     with conn:
+#         with conn.cursor as cur:
+#             with open('north_data/employees_data.csv') as file:
+#                 data = [tuple(line) for line in csv.reader(file)]  # список кортежей
+#                 for item in range(1, len(data)):
+#                     # execute query
+#                     cur.execute("INSERT TO employees VALUES (%s, %s, %s, %s, %s)", data[item])
+# finally:
+#     conn.close()
